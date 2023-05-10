3aa92d01ff6cdd58d317fc062d804b7af780bdc0
vikash <coolvikash9572@gmail.com>
2023-05-11 01:20:40 +0530

    Createcityrepo again

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const mysql = require("mysql2");
 // const {City} = require("./models/index");
 const {PORT}=require("./config/serviceconfig");
 
-const CityRepository = require("./repository/city_repo")
+
 
 
 const setupAndStartServer= async () => {
@@ -17,8 +17,7 @@ const setupAndStartServer= async () => {
 
    app.listen(PORT, () => {
       console.log(`Server started at  ${PORT}`);
-      const repo = new CityRepository;
-      repo.deleteCity(1);
+     
   
    });
 
