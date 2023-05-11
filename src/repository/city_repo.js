5829d6e7d2d45a459cb89ec8bf0d5b89a3127208
vikash <coolvikash9572@gmail.com>
2023-05-11 14:22:05 +0530

    Completed the city repo

--- src/repository/city_repo.js
+++ src/repository/city_repo.js
@@ -1,29 +1,58 @@
-const {City} = require("../models/index");
+const { City } = require("../models/index");
 
 
 class CityRepository {
-    async createCity ( {name }){
+    async createCity({ name }) {
         try {
-             const city = await City.create({name});
-             return city;
-        } catch (error){
-            throw {error};
-        }
-    }
-   
-    async deleteCity(cityID){
-        try {
-             await City.destroy({
-             where : {
-                id : cityID
-             }
-             })
-        } catch(error)
-        {
-            throw {error};
+            const city = await City.create({ name });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repo layer")
+            throw { error };
         }
     }
 
+    async deleteCity(cityID) {
+        try {
+            await City.destroy({
+                where: {
+                    id: cityID
+                }
+            });
+
+            return true;
+        } catch (error) {
+            console.log("Something went wrong in the repo layer")
+            throw { error };
+        }
+    }
+
+
+    async updateCity(cityID, data) {
+        try {
+            const city = await City.update(data, {
+                where: {
+                    id: cityID
+                }
+            });
+
+            return city;
+
+        } catch (error) {
+            console.log("Something went wrong in the repo layer")
+            throw { error };
+        }
+    }
+
+    async getCity(cityID) {
+        try {
+            const city = await City.findByPk(cityID);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repo layer")
+            throw { error };
+        }
+    }
 }
 
 module.exports = CityRepository;
