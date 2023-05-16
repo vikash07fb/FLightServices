ff4568cadf95a8b46a4f9b2509c899f93944dee7
vikash <coolvikash9572@gmail.com>
2023-05-17 00:53:08 +0530

    Added the flight middleware

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,6 +4,9 @@ const CityController = require("../../controllers/city_controller");
 const FlightsController = require("../../controllers/flight_controller")
 const AirportController = require("../../controllers/airport_controller");
 
+
+const {validateCreateFlight} = require("../../middlewares/flightMiddleware");
+
 const router = express.Router();
 
 // routes of city model
@@ -16,7 +19,7 @@ router.post('/cities',CityController.createMultipleCities);
 
 
 // routes of flights model
-router.post('/flights',FlightsController.createFlight);
+router.post('/flights',validateCreateFlight,FlightsController.createFlight);
 router.get('/flights',FlightsController.getAllFlight);
 
 
